ca5f8bdf12e87beaeaa88a7d1980d1ecc13a47a4
Rosemary De Luca <de.luca.4139@gmail.com>
2025-05-13 12:19:09 -0700

    Basic functional multiplayer
    After many iterations with different frameworks, I finally settled on Godot's built-in RPC solution for networking. This is a barebones functional example, utilizing RPC calls for a basic chat room. Will be expanded upon soon.

--- scripts/multiplayer_control.gd
+++ scripts/multiplayer_control.gd
@@ -1,9 +1,4 @@
-extends Control
-
-# These signals can be connected to by a UI lobby scene or the game scene.
-signal player_connected(peer_id, player_info)
-signal player_disconnected(peer_id)
-signal server_disconnected
+extends Node
 
 const PORT = 1337
 const DEFAULT_SERVER_IP = "127.0.0.1" # IPv4 localhost
@@ -12,113 +7,74 @@ const MAX_CONNECTIONS = 20
 # This will contain player info for every player,
 # with the keys being each player's unique IDs.
 var players = {}
-
-# This is the local player info. This should be modified locally
-# before the connection is made. It will be passed to every other peer.
-# For example, the value of "name" can be set to something the player
-# entered in a UI scene.
 var player_info = {"name": "Name"}
 
-var players_loaded = 0
-
 var peer = ENetMultiplayerPeer.new()
-@export var player_scene: PackedScene # Possible needed?
+#@export var player_scene: PackedScene # Possibly unneeded?
 
-# ID should eventually be updated to match
-# some specification that can be documented
-func add_player(id: int = 1):
-	var player = player_scene.instantiate()
-	player.name = str(id)
-	call_deferred("add_child", player)
-	player_connected.emit(1, player_info)
+func _complete_setup() -> void:
+	multiplayer.multiplayer_peer = peer 
+	$"Chat Entry".editable = true
+	$"Chat Send".disabled = false
 
 func _on_host_button_pressed():
+	print("Beginning host...")
 	var error = peer.create_server(PORT, MAX_CONNECTIONS)
 	if error:
 		return error
-	multiplayer.multiplayer_peer = peer
-	$"Start Button".visible = true
-	add_player()
-
+	_complete_setup()
+	
 func _on_join_button_pressed(address = ""):
+	print("Beginning join...")
+	if $Username.text: player_info["name"] = $Username.text
 	if address.is_empty():
 		address = DEFAULT_SERVER_IP
 	var error = peer.create_client(address, PORT)
 	if error:
 		return error
-	multiplayer.multiplayer_peer = peer
+	_complete_setup()
+
+# Update connected players' databases
+# when someone changes their name.
+func _on_username_text_changed(new_text: String) -> void:
+	# this functions but returns an
+	# error nullptr; can fix?
+	if not peer.host: return 
+	#if not multiplayer.multiplayer_peer: return
+	_register_player.rpc(new_text)
+
+func _on_chat_send_pressed() -> void:
+	send_chat_message.rpc($"Chat Entry".text)
+	$"Chat Entry".clear()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# These should also hook into signals,
+	# which can be linked to UI elements, etc
+	multiplayer.peer_connected.connect(func(id):
+		_register_player.rpc_id(id, $Username.text)
+		print("Peer connected. ID: %s" % id)
+	)
+	multiplayer.peer_disconnected.connect(func(id):
+		print("Peer disconnected. ID: %s" % id)
+	)
+
+@rpc("any_peer", "call_local", "unreliable_ordered")
+func _register_player(player_data):
+	print("Registering player...")
+	var player_id = multiplayer.get_remote_sender_id()
+	players[player_id] = player_data
+
+@rpc("any_peer", "call_local", "unreliable_ordered")
+func send_chat_message(text: String):
+	var username: String
+	if multiplayer.get_remote_sender_id() in players:
+		username = players[multiplayer.get_remote_sender_id()]
+	else:
+		# This is a failsafe for a bug where the
+		# client hasn't updated their name in 
+		# the local database yet. 
+		username = $Username.text
+		players[multiplayer.get_unique_id()] = username
 	
-func _on_start_button_pressed() -> void:
-	if multiplayer.is_server():
-		start_game.rpc()
-
-
-
-func _ready():
-	multiplayer.peer_connected.connect(_on_player_connected)
-	multiplayer.peer_disconnected.connect(_on_player_disconnected)
-	multiplayer.connected_to_server.connect(_on_connected_ok)
-	multiplayer.connection_failed.connect(_on_connected_fail)
-	multiplayer.server_disconnected.connect(_on_server_disconnected)
-
-
-
-
-
-## When the server decides to start the game from a UI scene,
-## do Lobby.load_game.rpc(filepath)
-#@rpc("call_local", "reliable")
-#func load_game(game_scene_path):
-	#get_tree().change_scene_to_file(game_scene_path)
-#
-#
-## Every peer will call this when they have loaded the game scene.
-#@rpc("any_peer", "call_local", "reliable")
-#func player_loaded():
-	#if multiplayer.is_server():
-		#players_loaded += 1
-		##if players_loaded == players.size():
-			##$/root/Game.start_game()
-			##players_loaded = 0
-
-
-# When a peer connects, send them my player info.
-# This allows transfer of all desired data for each player, not only the unique ID.
-func _on_player_connected(id):
-	print("joined! ~ " + str(id))
-	_register_player.rpc_id(id, player_info)
-
-
-@rpc("any_peer", "reliable")
-func _register_player(new_player_info):
-	var new_player_id = multiplayer.get_remote_sender_id()
-	players[new_player_id] = new_player_info
-	player_connected.emit(new_player_id, new_player_info)
-	
-@rpc("any_peer", "reliable")
-func start_game():
-	print("woag! " + str(multiplayer.get_remote_sender_id()))
-
-func remove_multiplayer_peer():
-	multiplayer.multiplayer_peer = null
-
-func _on_player_disconnected(id):
-	players.erase(id)
-	player_disconnected.emit(id)
-	print("oh noag")
-
-
-func _on_connected_ok():
-	var peer_id = multiplayer.get_unique_id()
-	players[peer_id] = player_info
-	player_connected.emit(peer_id, player_info)
-
-
-func _on_connected_fail():
-	multiplayer.multiplayer_peer = null
-
-
-func _on_server_disconnected():
-	multiplayer.multiplayer_peer = null
-	players.clear()
-	server_disconnected.emit()
+	$"Chat History".text += "(%s) %s\n" % [username, text]
